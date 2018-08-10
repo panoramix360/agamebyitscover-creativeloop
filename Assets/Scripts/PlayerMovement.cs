366b138ba4ec03859ae88ce704618acffc6c82e7
Lucas Reis <lucasreis3000@gmail.com>
2018-08-10 01:40:40 -0300

    Adding sprites and refactoring scripts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,19 +2,13 @@
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float scaleMinLimit = 6f;
-    public float scaleMaxLimit = 12f;
-    public float offset = 3f;
-
     public float speed = 8f;
 
-    private Transform transform;
     private Rigidbody2D rigidbody;
     private float initialSpeed;
 
     private void Awake()
     {
-        transform = GetComponent<Transform>();
         rigidbody = GetComponent<Rigidbody2D>();
 
         rigidbody.AddTorque(Random.Range(-20f, 20f));
@@ -26,19 +20,7 @@ public class PlayerMovement : MonoBehaviour {
     {
         float vertical = Input.GetAxis("Vertical");
 
-        HandleMovement(vertical);
-        HandleDistance();
-    }
-
-    private void HandleDistance()
-    {
-        float offsetSpan = scaleMaxLimit - scaleMinLimit;
-        float positionOnScreen = Mathf.Abs(Camera.main.WorldToScreenPoint(transform.position).y - Screen.height);
-        float positionOnScreenNormalized = (positionOnScreen - scaleMinLimit) / offsetSpan;
-
-        float scaleX = Mathf.Clamp(positionOnScreenNormalized + offset, scaleMinLimit, scaleMaxLimit);
-        float scaleY = Mathf.Clamp(positionOnScreenNormalized + offset, scaleMinLimit, scaleMaxLimit);
-        transform.localScale = new Vector3(scaleX, scaleY, 0f);
+        HandleMovement(vertical);   
     }
 
     private void HandleMovement(float verticalInput)
@@ -53,8 +35,5 @@ public class PlayerMovement : MonoBehaviour {
         }
 
         rigidbody.AddForce(new Vector2(0f, speed * verticalInput));
-
-        Debug.Log(rigidbody.velocity);
-        
     }
 }
---
+++ Assets/Scripts/ScaleBasedOnY.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ScaleBasedOnY : MonoBehaviour {
+
+    public float scaleMinLimit = 2.7f;
+    public float scaleMaxLimit = 5f;
+    public float offset = -12f;
+
+    private Transform transform;
+
+    private void Awake()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        HandleDistance();
+    }
+
+    private void HandleDistance()
+    {
+        float offsetSpan = scaleMaxLimit - scaleMinLimit;
+        float positionOnScreen = Mathf.Abs(Camera.main.WorldToScreenPoint(transform.position).y - Screen.height);
+        float positionOnScreenNormalized = (positionOnScreen - scaleMinLimit) / offsetSpan;
+
+        Debug.Log(positionOnScreenNormalized);
+
+        float scaleX = Mathf.Clamp(positionOnScreenNormalized + offset, scaleMinLimit, scaleMaxLimit);
+        float scaleY = Mathf.Clamp(positionOnScreenNormalized + offset, scaleMinLimit, scaleMaxLimit);
+        transform.localScale = new Vector3(scaleX, scaleY, 0f);
+    }
+}
