175727d7e4b32465676c23eef8680f4fcc0176eb
Lucas Reis <lucasreis3000@gmail.com>
2018-09-14 02:58:25 -0300

    Creating intro

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour {
+
+    [SerializeField]
+    private SceneFader sceneFader;
+
+    private void Start()
+    {
+        StartCoroutine(PlayGame());
+    }
+
+    private IEnumerator PlayGame()
+    {
+        yield return new WaitForSeconds(6f);
+        sceneFader.FadeTo("MainScene");
+    }
+}
